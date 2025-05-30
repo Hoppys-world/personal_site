e8937dcb7d0b5b8c4cd8e8d19039f7332ebbbf6d
Hoppys-world <89815983+Hoppys-world@users.noreply.github.com>
2025-05-29 19:06:04 -0600

    General scaffolding of page

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,53 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const scrollToSection = (id) => {
+    const section = document.getElementById(id);
+    if (section) {
+      section.scrollIntoView({ behavior: "smooth" });
+    }
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    
+    <div className="App">
+      <header className="banner">
+        <nav>
+          <button onClick={() => scrollToSection("section1")}>About</button>
+          <button onClick={() => scrollToSection("section2")}>Work</button>
+          <button onClick={() => scrollToSection("section3")}>Skills</button>
+          <button onClick={() => scrollToSection("section4")}>Contact</button>
+        </nav>
+      </header>
+
+      <section id="section0" className="head-section">
+        <h1>I'm Jacob Hopkins,</h1>
+        <p>A Full Stack developer from Salt Lake</p>
+      </section>
+
+      <section id="section1" className="section">
+        <h2>About</h2>
+        <p>This is the first section of the page.</p>
+      </section>
+
+      <section id="section2" className="section">
+        <h2>Work</h2>
+        <p>This is the second section of the page.</p>
+      </section>
+
+      <section id="section3" className="section">
+        <h2>Skills</h2>
+        <p>This is the third section of the page.</p>
+      </section>
+
+      <section id="section4" className="section">
+        <h2>Contact</h2>
+        <p>This is the third section of the page.</p>
+      </section>
+    </div>
+  );
 }
 
-export default App
+export default App;
+
